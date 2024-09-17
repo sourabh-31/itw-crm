7685788660d2a6489efc5450a8402a0b632063e2
Sourabh <sourabhhaldarh@gmail.com>
2024-09-17 19:16:14 +0530

    style fix

--- src/components/shared/Feed.tsx
+++ src/components/shared/Feed.tsx
@@ -4,8 +4,6 @@ import Lottie from "lottie-web";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
 
-import { cn } from "@/lib/utils";
-
 interface FeedPropsType {
   topic: string;
   description: string;
@@ -82,60 +80,49 @@ export default function Feed({
 
   return (
     <div
+      className="card transition-transform duration-300 ease-out"
       ref={feedRef}
-      className={cn(
-        "flex",
-        "transition-transform duration-300 ease-out",
-        "hover:shadow-xl",
-        className
-      )}
       style={{ transformStyle: "preserve-3d" }}
     >
-      <div>
-        <div
-          className={cn(
-            `flex h-12 w-60 items-center rounded-[25px_50px_0px_0px] border border-b-0 bg-primary-400 pl-4 font-recoletaAlt text-yellow-100 ${isBorder ? "border-[#0094FF]" : "border-none"}`,
-            topicClassName
-          )}
-        >
-          {topic}
-        </div>
-        <div
-          className={`h-[8.4rem] w-60 rounded-[0px_0px_0px_30px] border border-r-0 border-t-0 bg-primary-400 pl-5 pr-2 ${isBorder ? "border-[#0094FF]" : "border-none"}`}
-        >
-          <span className="font-mulish">{description}</span>
-
-          {isActionBtn && (
+      <div className="box shimmer-feed">
+        <div className="icon">
+          <div className="iconBox">
             <Link
               href="/"
-              className="mt-5 flex w-fit items-center justify-center rounded-full bg-blue px-7 py-[0.65rem] font-mulish text-sm transition-transform duration-300 ease-out hover:scale-105"
+              className="absolute left-2 top-0 flex h-9 w-24 items-center justify-center rounded-[30px] border border-gray-light bg-primary-400 transition-transform duration-300 ease-out hover:scale-105"
             >
-              OPEN
+              {isBtnText ? (
+                <div className="font-mulish text-sm">
+                  <span>Send</span>
+                  <span>👋</span>
+                </div>
+              ) : (
+                <span className="-ml-1 -mt-1">👋</span>
+              )}
             </Link>
-          )}
+          </div>
         </div>
-      </div>
-
-      <div className="relative">
-        <div
-          className={`absolute bottom-0 flex h-[8.4rem] w-28 items-center justify-center self-end rounded-r-[30px] border border-l-0 bg-primary-400 ${isBorder ? "border-[#0094FF]" : "border-none"}`}
-        >
-          <div ref={animationContainer} className="w-16" />
-        </div>
-
-        <Link
-          href="/"
-          className="absolute left-2 top-0 flex h-9 w-24 items-center justify-center rounded-[30px] border border-gray-light bg-primary-400 transition-transform duration-300 ease-out hover:scale-105"
-        >
-          {isBtnText ? (
-            <div className="font-mulish text-sm">
-              <span>Send</span>
-              <span>👋</span>
+        <div className="content">
+          <div>
+            <div className="pl-5 pt-4 font-recoletaAlt text-yellow-100">
+              {topic}
             </div>
-          ) : (
-            <span className="-ml-1 -mt-1">👋</span>
-          )}
-        </Link>
+            <div className="mt-3 w-3/5 pl-5 font-mulish">{description}</div>
+
+            {isActionBtn && (
+              <Link
+                href="/"
+                className="ml-4 mt-7 flex w-fit items-center justify-center rounded-full bg-blue px-7 py-[0.65rem] font-mulish text-sm transition-transform duration-300 ease-out hover:scale-105"
+              >
+                OPEN
+              </Link>
+            )}
+          </div>
+          <div
+            ref={animationContainer}
+            className="absolute bottom-8 right-8 w-16"
+          />
+        </div>
       </div>
     </div>
   );
