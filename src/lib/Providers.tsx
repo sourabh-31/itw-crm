5006e06cfb80be5892e9daec5e4e83b7afc276a7
Sourabh <sourabhhaldarh@gmail.com>
2024-09-27 13:43:18 +0530

    REFACTOR: react query devtool

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { ReactNode } from "react";
 
 const queryClient = new QueryClient({
@@ -18,7 +18,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+      {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" /> */}
     </QueryClientProvider>
   );
 }
