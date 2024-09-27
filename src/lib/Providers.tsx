8186a77e2bcf8edfd947ffe3f468dffa2a7c313e
Sourabh <sourabhhaldarh@gmail.com>
2024-09-27 10:55:03 +0530

    FIX: network bug

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,15 +1,13 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { ReactNode } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      staleTime: 30 * 60 * 1000,
+      staleTime: 0,
     },
   },
 });
@@ -18,7 +16,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+      {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" /> */}
     </QueryClientProvider>
   );
 }
