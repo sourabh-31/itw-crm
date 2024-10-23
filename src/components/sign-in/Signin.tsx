a87f1f050162f23dc8b876d4e5aad733f39d8b5c
Sourabh <sourabhhaldarh@gmail.com>
2024-10-23 17:04:05 +0530

    FIX: google oauth

--- src/components/sign-in/Signin.tsx
+++ src/components/sign-in/Signin.tsx
@@ -16,7 +16,6 @@ export default function Signin() {
 
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => setAccessToken(codeResponse.access_token),
-    flow: "implicit",
   });
 
   useEffect(() => {
